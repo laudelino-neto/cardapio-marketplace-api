3a222b6e76b520d9c7ea685d638cf36585100983
Laudelino M C Neto <laudenpower@gmail.com>
2023-08-31 22:08:09 -0300

    Finalizado o mapeamento da classe Restaurante e a criação do respectivo repository

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -1,21 +1,32 @@
 package br.com.senai.cardapiosmktplaceapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
+import br.com.senai.cardapiosmktplaceapi.entity.Endereco;
+import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
+import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
 import br.com.senai.cardapiosmktplaceapi.entity.enums.TipoDeCategoria;
 import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
+import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
 
 @SpringBootApplication
 public class InitApp {
 	
 	@Autowired
-	private CategoriasRepository repository;
+	private CategoriasRepository categoriasRepository;
+	
+	@Autowired
+	private RestaurantesRepository restaurantesRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
@@ -24,15 +35,12 @@ public class InitApp {
 	@Bean
 	public CommandLineRunner commandLineRuner(ApplicationContext ctx) {
 		return args -> {
-			/*Categoria novaCategoria = new Categoria();
-			novaCategoria.setNome("Temakeria");
-			novaCategoria.setTipo(TipoDeCategoria.RESTAURANTE);
-			this.repository.save(novaCategoria);*/
-			Categoria categoria = repository.buscarPor(5);
-			System.err.println(categoria.getNome());
-			categoria.setNome("Novo nome da categoria ");
-			this.repository.save(categoria);
-			System.out.println("Zé FINI");
+			Categoria categoria = categoriasRepository.buscarPor(39);
+			List<Restaurante> restaurantes = restaurantesRepository.listarPor("%rest%", 
+					categoria, PageRequest.of(0, 15)).getContent();
+			for (Restaurante r : restaurantes) {
+				System.out.println(r.getNome() + " - " + r.getCategoria().getNome());
+			}
 		};
 	}
 	
