ff0371a0f50bef3155fef4c6d96fb107d2e2edbe
Laudelino M C Neto <laudenpower@gmail.com>
2023-09-05 22:12:52 -0300

    Finalização dos services e categoria e restaurante

--- src/main/java/br/com/senai/cardapiosmktplaceapi/repository/CardapiosRepository.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/repository/CardapiosRepository.java
@@ -1,11 +1,18 @@
 package br.com.senai.cardapiosmktplaceapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.senai.cardapiosmktplaceapi.entity.Cardapio;
 
 @Repository
 public interface CardapiosRepository extends JpaRepository<Cardapio, Integer>{
+	
+	@Query(value = 
+			"SELECT Count(c) "
+			+ "FROM Cardapio c "
+			+ "WHERE c.restaurante.id = :idDoRestaurante ")
+	public Long contarPor(Integer idDoRestaurante);
 
 }
