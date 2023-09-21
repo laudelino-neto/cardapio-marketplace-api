87bb5092eeb4993cadfcad70e8b34ca8b92026fb
Laudelino M C Neto <laudenpower@gmail.com>
2023-09-20 22:09:17 -0300

    Inicio do controller de cardapios

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -1,34 +1,15 @@
 package br.com.senai.cardapiosmktplaceapi;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 
-import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
-import br.com.senai.cardapiosmktplaceapi.entity.Endereco;
-import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
-import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
-import br.com.senai.cardapiosmktplaceapi.entity.enums.TipoDeCategoria;
-import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
-import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
-
 @SpringBootApplication
 public class InitApp {
-	
-	@Autowired
-	private CategoriasRepository categoriasRepository;
-	
-	@Autowired
-	private RestaurantesRepository restaurantesRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
@@ -42,7 +23,7 @@ public class InitApp {
 	@Bean
 	public CommandLineRunner commandLineRuner(ApplicationContext ctx) {
 		return args -> {
-			System.out.println("Subiu");
+
 		};
 	}
 	
